5546850d4bbc7430890b142f2c7aa06bb765596c
Explie <tobias@dfir-dd.de>
2024-10-30 16:57:31 +0100

    Switched to GNOME Wayland

--- Kali-2024/scripts/base-tools.sh
+++ Kali-2024/scripts/base-tools.sh
@@ -2,3 +2,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get install -yq clang libssl-dev flex bison libscca-utils libscca-dev eza pipx
+
+# switching to wayland
+mkdir -p /etc/systemd/system/gdm.service.d
+ln -sf /dev/null /etc/systemd/system/gdm.service.d/disable-wayland.conf
