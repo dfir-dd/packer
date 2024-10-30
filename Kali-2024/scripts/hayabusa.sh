132cff8d32ee0a693c4d5ebfcb5f69ef1fd0ab69
Explie <tobias@dfir-dd.de>
2024-10-30 16:57:04 +0100

    fixed hayabusa install

--- Kali-2024/scripts/hayabusa.sh
+++ Kali-2024/scripts/hayabusa.sh
@@ -1,6 +1,6 @@
  #!/usr/bin/env bash
  if [ ! -x /opt/hayabusa/README.md ]; then
-      URL=$(curl -s https://api.github.com/repos/yamato-security/hayabusa/releases/latest | grep "browser_download_url" | grep "linux" | sed 's/.*: "//' | rev | cut -c2- | rev)  && wget $URL -O /tmp/hayabusa.zip
+      URL=$(curl -s https://api.github.com/repos/yamato-security/hayabusa/releases/latest | grep "browser_download_url" | grep "lin-x64-gnu" | sed 's/.*: "//' | rev | cut -c2- | rev)  && wget $URL -O /tmp/hayabusa.zip
       set +e
       unzip /tmp/hayabusa.zip -d /opt/hayabusa
       set -e
